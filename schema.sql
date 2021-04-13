871c399e98c3e60342f98b66acfd80d0cd473cc5
naisthorpe <aisthorpe@gmail.com>
2021-04-13 19:59:48 -0400

    Change role table to position

--- schema.sql
+++ schema.sql
@@ -6,14 +6,14 @@ CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL,
+    position_id INT NOT NULL,
     manager_id INT NULL,
     PRIMARY KEY (id),
-    FOREIGN KEY (role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES role(id)
+    FOREIGN KEY (position_id) REFERENCES position(id),
+    FOREIGN KEY (manager_id) REFERENCES position(id)
 );
 
-CREATE TABLE role (
+CREATE TABLE position (
     id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(10,2) NOT NULL,
---
+++ seed.sql
@@ -0,0 +1,13 @@
+USE employee_trackerDB;
+
+INSERT INTO employee (first_name, last_name)
+VALUES ("Nick", "Aisthorpe"),
+    ("Brittany", "Elliott");
+
+INSERT INTO role (title, salary)
+VALUES ("Boss", "120000"),
+    ("Minion", "40000");
+
+INSERT INTO department (name)
+VALUES ("Toys"),
+    ("Food");
